cc90124d2e0d543aef73bb75eb0c0835e1433542
jiabin <1123565780@qq.com>
2022-09-03 17:30:53 +0800

    update

--- Vue2Projects/day1/change-rows-color/src/index.js
+++ Vue2Projects/day1/change-rows-color/src/index.js
@@ -1,12 +1,13 @@
 import $ from 'jquery'    // es6高级语法，目前没有引擎能解析import，所以都要通过webpack进行兼容性处理
 import './css/index.css'    // 如果某个模块中，用from接受到的变量为undefined ，则没必要接收，仅导入即可
-import logo from '../image/msgcenter_close.png'     // 把图片读取为base64字符串，
-$('img').attr('src', logo);    // 给img的src属性赋值base64字符串，这样仅需读html文件即可同时得到图片，而不需要根据图片的url再次访问服务器
+import logo from '../images/msgcenter_close.png'     // 把图片读取为base64字符串，
+$('.box').attr('src', logo);    // 给img的src属性赋值base64字符串，这样仅需读html文件即可同时得到图片，而不需要根据图片的url再次访问服务器
 $(function () {
     $('li:odd').css('background-color', 'red')
     $('li:even').css('background-color', 'skyblue')
 
 })
+consle.log('ss');
 // npm install命令安装包时，是把包安装在与package.json同目录下的node_modules文件夹里的
 // 所以说，先npm init -y 在相应的目录下生成package.json，再安装包
 
--- Vue2Projects/day1/change-rows-color/webpack.config.js
+++ Vue2Projects/day1/change-rows-color/webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');    //首字母大写的都是构造函数，需要用它实例化一个对象
 const htmlPlugin = new HtmlPlugin({   //该插件负责将src里的index.html复制一份到项目根目录里，从而打开链接就是网页
     template: './src/index.html',
     filename: './index.html'
 });
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, './src/index.js'),     //entry是入口，即webpack脚本要处理的文件路径，默认是./src/index.js
     output: {                                          //ouput设置经webpack脚本处理后的文件存放在什么位置 ，默认是./dist/main.js
         path: path.join(__dirname, './dist'),   //存放的目录
-        filename: 'js/bundle.js'    //存放的文件名
+        filename: 'js/bundle.js'    //存放的文件夹和文件名
     },
-    plugins: [htmlPlugin],  //plugins数组里面存放各个插件
+    plugins: [htmlPlugin, new CleanWebpackPlugin()],  //plugins数组里面存放各个插件
     devServer: {
         open: true,   // 首次打包后自动打开浏览器
         port: 80,      // 还可以自定义端口号
@@ -22,12 +23,19 @@ module.exports = {
         rules: [
             //实现用loader协助webpack把css文件也打包到boudle.js文件中，因为webpack自己只会打包js文件
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            // 用loader协助webpack把img文件导入并转为base64字符串，limit表示<22229字节的图片才转base64，大图片不转，因为体积会变大
-            { test: /\.png|jpg|gif$/, use: 'url-loader?limit=10000&outputPath=images' }
+            // 用loader协助webpack把img文件导入并转为base64字符串，limit表示<22229字节的图片才转base64，大图片不转，因为体积反而会变大
+            { test: /\.png|jpg|gif$/, use: 'url-loader?limit=20000&outputPath=images' }
         ]
     },
+    // devtool: 'eval-source-map',    //使报错的行号是源码文件的行号，而不是bundle.js的行号。开发时打开，上线时关闭
+    devtool: 'nosources-source-map',   //上线时建议使用这个，只暴露源码行号不暴露源码，完美！
     mode: 'production'    //意为当前项目所处模式是什么，如果是在开发阶段则设置为development，如果处于项目上线阶段则设置为production
 }
 
 // development的打包速度快，但是体积大。由于开发时追求打包速度快，所以用dev
 // production的打包体积小，但是速度慢。由于项目上线追求代码体积小，所以用pro。体积小是因为会把代码压缩处理
+
+
+
+// webpack是npm的工具模块,是一个JS应用打包器, 它将应用中的各个模块打包成一个或者多个bundle文件。借助loaders和plugins，它可以改变、压缩和优化各种各样的文件。
+// 输入不同资源，比如：html、css、js、img、font文件等，然后将它们输出浏览器可以正常解析的文件。
