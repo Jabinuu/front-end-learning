fc6bf9f6602bb514d41b30401ef1eb64c00bfef6
jiabin <1123565780@qq.com>
2022-09-03 11:25:12 +0800

    update

--- Vue2Projects/day1/change-rows-color/webpack.config.js
+++ Vue2Projects/day1/change-rows-color/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: path.join(__dirname, './src/index.js'),     //entry是入口，即webpack脚本要处理的文件路径，默认是./src/index.js
     output: {                                          //ouput设置经webpack脚本处理后的文件存放在什么位置 ，默认是./dist/main.js
         path: path.join(__dirname, './dist'),   //存放的目录
-        filename: 'bundle.js'    //存放的文件名
+        filename: 'js/bundle.js'    //存放的文件名
     },
     plugins: [htmlPlugin],  //plugins数组里面存放各个插件
     devServer: {
@@ -23,7 +23,7 @@ module.exports = {
             //实现用loader协助webpack把css文件也打包到boudle.js文件中，因为webpack自己只会打包js文件
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             // 用loader协助webpack把img文件导入并转为base64字符串，limit表示<22229字节的图片才转base64，大图片不转，因为体积会变大
-            { test: /\.png|jpg|gif$/, use: ['url-loader?limit = 22229'] }
+            { test: /\.png|jpg|gif$/, use: 'url-loader?limit=10000&outputPath=images' }
         ]
     },
     mode: 'production'    //意为当前项目所处模式是什么，如果是在开发阶段则设置为development，如果处于项目上线阶段则设置为production
