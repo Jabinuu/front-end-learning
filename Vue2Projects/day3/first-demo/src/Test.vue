4e0cd088dd97892e8ab2a9499d493dbf638093ae
jiabin <1123565780@qq.com>
2022-09-13 23:02:40 +0800

    update

---
+++ Vue2Projects/day3/first-demo/src/Test.vue
@@ -0,0 +1,33 @@
+// tmeplate里写组件
+<template>
+  <div>
+    <h1>这是用户自定义.vue文件---{{ username }}</h1>
+    <h2>ssss</h2>
+  </div>
+</template>
+
+// script写操作组件的代码
+<script>
+export default {
+  // 默认导出，固定写法
+  // 注意：.vue组件里的data不能像之前那样，给data赋值对象
+  //   .vue里data应该是一个函数，return一个数据源对象
+  data() {
+    return {
+      username: "jiabin",
+    };
+  },
+  methods: {},
+  watch: {},
+  computed: {},
+  filters: {},
+};
+</script>
+
+// style里写样式
+<style lang="less">
+// 加上lang="less" 才能启用less语句
+h1 {
+  background: pink;
+}
+</style>
--- Vue2Projects/day3/first-demo/src/main.js
+++ Vue2Projects/day3/first-demo/src/main.js
@@ -1,8 +1,15 @@
+// 导入vue这个包，得到Vue构造函数
 import Vue from 'vue'
+
+// 导入 App.vue 根组件，将来要把 APp.vue 中的模板结构，渲染到 HTML页面中
 import App from './App.vue'
+import Test from './Test.vue'
 
 Vue.config.productionTip = false
 
+// 创建vue实例
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  // el: '#app',
+  // 把 render属性指定的组件。渲染到HTML页面中，替换掉index.html的<div id="app"></div>
+  render: h => h(Test),
+}).$mount('#app')   // 作用与el属性完全一样
