58a76b971624b9ffd51f77fb5e47936ef04b9157
jiabin <1123565780@qq.com>
2022-09-15 11:16:10 +0800

    update

---
+++ Vue2Projects/day4/demo-1/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr />
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <Left> </Left>
+      <Right> </Right>
+    </div>
+  </div>
+</template>
+
+<script>
+import Left from "@/components/Left.vue";
+import Right from "@/components/Right.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  // 注册组件，注册完就可以当标签用了
+  components: {
+    // Left:Left   键值对同名的话，可以简写成如下
+    Left,
+    Right,
+  },
+};
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
