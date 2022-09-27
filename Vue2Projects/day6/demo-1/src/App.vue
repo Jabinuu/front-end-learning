f652b38c74db76ce48a367f6c17933440963aeff
jiabin <1123565780@qq.com>
2022-09-27 14:10:35 +0800

    update

---
+++ Vue2Projects/day6/demo-1/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="app-container">
+        <h1>App 根组件</h1>
+        <hr />
+
+        <button @click="comName = 'Left'">渲染Left</button>
+        <button @click="comName = 'Right'">渲染Right</button>
+        <div class="box">
+            <!-- 1. component标签是vue内置的，作用：组件的占位符 -->
+            <!-- 2. is属性的值，表示要渲染的组件的名字 -->
+            <!-- 3. is属性的值，应该是组件在components节点下注册的名称 -->
+
+            <!-- keep-alive可以把内部的组件进行缓存，而不是销毁组件 -->
+            <!-- 在使用keep-alive的时候，可以通过include指定哪些组件需要被缓存 -->
+            <!-- 或者通过exclude属性指定哪些组件不需要被缓存。但是，不要同时用 -->
+            <keep-alive include="MyRight,MyLeft">
+                <component :is="comName"></component>
+            </keep-alive>
+        </div>
+    </div>
+</template>
+
+<script>
+import Left from "@/components/Left.vue";
+import Right from "@/components/Right.vue";
+export default {
+    data() {
+        return {
+            comName: "",
+        };
+    },
+    // 组件的名称默认为注册时的名称。可以在组件的name节点自定义他的名称
+    components: {
+        Left,
+        Right,
+    },
+};
+</script>
+
+<style lang="less">
+.app-container {
+    padding: 1px 20px 20px;
+    background-color: #efefef;
+}
+.box {
+    display: flex;
+}
+</style>
---
+++ Vue2Projects/day6/demo-1/src/components/Left.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="left-container">
+        <h3>Left 组件--{{ count }}</h3>
+        <button @click="count++">+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyLeft",
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    created() {
+        console.log("left组件被创建");
+    },
+    destroyed() {
+        console.log("Left组件被销毁");
+    },
+
+    // 当组件第一次被创建的是偶，既会执行created生命周期函数，也会执行activated生命周期
+    // 但是，当组件被激活的时候，只会触发activated生命周期，不再触发created，因为组件没有被重新创建
+    activated() {
+        console.log("Left组件被激活了");
+    },
+    deactivated() {
+        console.log("组件被缓存了");
+    },
+};
+</script>
+
+<style lang="less">
+.left-container {
+    padding: 0 20px 20px;
+    background-color: orange;
+    min-height: 250px;
+    flex: 1;
+}
+</style>
