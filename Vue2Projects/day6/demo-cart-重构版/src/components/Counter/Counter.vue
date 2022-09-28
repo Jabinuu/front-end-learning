2429d57aff40a0a463a541455d20d9da2bb4a211
jiabin <1123565780@qq.com>
2022-09-28 14:35:11 +0800

    update

--- Vue2Projects/day6/demo-cart-重构版/src/components/Counter/Counter.vue
+++ Vue2Projects/day6/demo-cart-重构版/src/components/Counter/Counter.vue
@@ -1,27 +1,53 @@
 <template>
-  <div class="number-container d-flex justify-content-center align-items-center">
-    <!-- 减 1 的按钮 -->
-    <button type="button" class="btn btn-light btn-sm">-</button>
-    <!-- 购买的数量 -->
-    <span class="number-box">1</span>
-    <!-- 加 1 的按钮 -->
-    <button type="button" class="btn btn-light btn-sm">+</button>
-  </div>
+    <div
+        class="
+            number-container
+            d-flex
+            justify-content-center
+            align-items-center
+        "
+    >
+        <!-- 减 1 的按钮 -->
+        <button type="button" class="btn btn-light btn-sm" @click="sub">
+            -
+        </button>
+        <!-- 购买的数量 -->
+        <span class="number-box">{{ count }}</span>
+        <!-- 加 1 的按钮 -->
+        <button type="button" class="btn btn-light btn-sm" @click="add">
+            +
+        </button>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    props: {
+        count: {
+            type: Number,
+            default: 0,
+        },
+    },
+    methods: {
+        add() {
+            this.$emit("countChange", this.count + 1);
+        },
+        sub() {
+            this.$emit("countChange", this.count - 1);
+        },
+    },
+};
 </script>
 
 <style lang="less" scoped>
 .number-box {
-  min-width: 30px;
-  text-align: center;
-  margin: 0 5px;
-  font-size: 12px;
+    min-width: 30px;
+    text-align: center;
+    margin: 0 5px;
+    font-size: 12px;
 }
 
 .btn-sm {
-  width: 30px;
+    width: 30px;
 }
 </style>
