3e1e2f0afa6288d61e1e8ba5be845c8b295134db
jiabin <1123565780@qq.com>
2022-09-29 10:07:10 +0800

    update

--- Vue2Projects/day7/router-demo1/src/App2.vue
+++ Vue2Projects/day7/router-demo1/src/App2.vue
@@ -2,14 +2,27 @@
   <div class="app-container">
     <h1>App2 组件</h1>
 
+    <!-- <a href="#/home">首页</a>
+    <a href="#/movie">电影</a>
+    <a href="#/about">关于</a> -->
+
+    <!--当你有了Vue Router时，使用router-link代替a标签是一个专业的写法 -->
+    <!-- 同样，这里的hash地址的#是不需要写的，运行时Vue Router会自动补上 -->
+    <router-link to="/home">首页</router-link>
+    <router-link to="/movie">电影</router-link>
+    <router-link to="/about">关于</router-link>
+
     <hr />
+
+    <!-- router-view是占位符，给经router路由模块选择到的组件使用 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style lang="less" scoped>
--- Vue2Projects/day7/router-demo1/src/router/index.js
+++ Vue2Projects/day7/router-demo1/src/router/index.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/components/Home.vue'
+import About from '@/components/About.vue'
+import Movie from '@/components/Movie.vue'
 
 // 通过Vue.use()给Vue安装vue-router插件
 Vue.use(VueRouter)
 // 向外暴露vue-router的实例
-export default new VueRouter(); 
+export default new VueRouter({
+    routes: [
+        // 重定向的路由规则
+        { path: '/', redirect: '/home' },
+
+        // 路由规则
+        // 与VueRouter有关的地方，写hash地址是不带#号的，只有/xxx
+        { path: '/home', component: Home },
+        { path: '/movie', component: Movie },
+        { path: '/about', component: About }
+    ]
+}); 
