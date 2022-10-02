1e8a907067bfc9fc22062543a0497a0da5a16570
jiabin <1123565780@qq.com>
2022-10-02 20:34:10 +0800

    update

--- Vue2Projects/day7/router-admin/src/router/index.js
+++ Vue2Projects/day7/router-admin/src/router/index.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MyLogin from '@/components/MyLogin.vue'
+import MyHome from '@/components/MyHome.vue'
 
 // 通过Vue.use()给Vue安装vue-router插件
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     routes: [
-        { path: '/login', component: MyLogin }
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: MyLogin },
+        { path: '/home', component: MyHome }
     ]
 })
 
