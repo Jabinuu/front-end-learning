80e93b02ebc0fafe866e017d73727a915cc8b031
jiabin <1123565780@qq.com>
2023-06-13 14:51:24 +0800

    新增手写create

--- JavaScriptProjects/手写create.js
+++ JavaScriptProjects/手写create.js
@@ -1,5 +1,29 @@
-function myCreate(prototype){
-  
-};
+function myCreate(prototype, propertiesDesc) {
+  if (typeof prototype !== "object" && typeof prototype !== "function") {
+    throw "error";
+  }
+  const res = new Object();
+  res.__proto__ = prototype;
+  if (propertiesDesc) {
+    Object.defineProperties(res, propertiesDesc);
+  }
+  return res;
+}
 
-const obj =  
+// 测试
+Object.prototype.myCreate = myCreate;
+const obj = Object.myCreate(null, {
+  name: {
+    configurable: true,
+    enumerable: true,
+    value: "jiabin",
+    writable: true,
+  },
+  age: {
+    configurable: true,
+    enumerable: true,
+    value: 23,
+    writable: true,
+  },
+});
+console.log(obj);
